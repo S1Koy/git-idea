1c0407e0502457d7daeda2e97efddce7bae7f787
S1Koy <50107918+S1Koy@users.noreply.github.com>
2022-03-23 14:56:13 +0800

    pull commit

--- src/main/java/com/szl/git/Test.java
+++ src/main/java/com/szl/git/Test.java
@@ -9,5 +9,6 @@ public class Test {
         System.out.println("master test!");
         System.out.println("hello git test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
